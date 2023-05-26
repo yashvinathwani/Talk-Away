b43d997de08e32aa487e105c70c2a80e0f2f7cdb
yashvinathwani <nathwaniyashvi@gmail.com>
2023-05-27 00:53:35 +0530

    Search User API

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -49,7 +49,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       pic: user.pic,
-      token: generateToken(user._id),
+      token: `Bearer ${generateToken(user._id)}`,
     });
   } else {
     res.status(401);
@@ -57,4 +57,18 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: 'i' } },
+          { email: { $regex: req.query.search, $options: 'i' } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
