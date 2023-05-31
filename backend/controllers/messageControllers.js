359b87d17670cd6acec7dc7634546a3e7259373c
yashvinathwani <nathwaniyashvi@gmail.com>
2023-06-01 00:28:37 +0530

    Send Message and Get All Chat Messages API

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,54 @@
+const asyncHandler = require('express-async-handler');
+const Message = require('../models/Message');
+const User = require('../models/User');
+const Chat = require('../models/Chat');
+
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log('Invalid data passed into the request');
+    return res.sendStatus(400);
+  }
+
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+
+    message = await message.populate('sender', 'name pic');
+    message = await message.populate('chat');
+    message = await User.populate(message, {
+      path: 'chat.users',
+      select: 'name pic email',
+    });
+
+    await Chat.findByIdAndUpdate(chatId, {
+      latestMessage: message,
+    });
+
+    res.status(200).json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    var messages = await Message.find({ chat: req.params.chatId })
+      .populate('sender', 'name pic email')
+      .populate('chat');
+
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { sendMessage, allMessages };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
