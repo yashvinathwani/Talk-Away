d00e7faaf412c551bb26bda57276c084d1eccec6
yashvinathwani <nathwaniyashvi@gmail.com>
2023-06-04 04:25:54 +0530

    Deployment Setup

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -19,7 +19,7 @@ import Lottie from 'react-lottie';
 import animationData from '../animations/typing.json';
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'https://talk-away-two.vercel.app/';
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
